f5a236cf2d9d1a5b7d11ab5208fa5bde67922750
Lucchetto <lucchetto.tie@live.com>
2020-08-09 09:29:11 +0200

    zippo: import libperfmgr
        * From https://android.googlesource.com/platform/hardware/google/pixel android-10.0.0_r41
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- common.mk
+++ common.mk
@@ -381,7 +381,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.3-service.pixel-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QMI
 PRODUCT_PACKAGES += \
