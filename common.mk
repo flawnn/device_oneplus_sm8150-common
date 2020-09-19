924672e9a1fbb4a5ceee0ebb750f447629c3a57a
Adhitya Mohan <me@adhityamohan.in>
2020-09-19 19:50:22 +0530

    sm8150-common: Avoid copying vndk 29 protobuf -- Apparently our blobs work fine with v30?

--- common.mk
+++ common.mk
@@ -480,10 +480,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.android.vndk.current.on_vendor
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
-
 PRODUCT_TARGET_VNDK_VERSION := 29
 PRODUCT_EXTRA_VNDK_VERSIONS := 29
 
