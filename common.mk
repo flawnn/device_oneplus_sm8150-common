36be365eb29baf7f95229004a67ff422fae418d9
baalajimaestro <me@baalajimaestro.me>
2021-04-30 11:38:41 +0530

    Import DeviceSettings and required sepolicy from PE
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- common.mk
+++ common.mk
@@ -229,6 +229,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# DeviceSettings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
