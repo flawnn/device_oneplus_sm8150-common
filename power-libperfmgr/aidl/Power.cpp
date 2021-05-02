PixelExperience-Devices/device_xiaomi_davinci

power-libperfmgr: Adapt and rebrand
Change-Id: I22203a8012ed468478a21810d5afdd82a5cdc053

power-libperfmgr/aidl/Power.cpp
@@ -15,7 +15,7 @@
  */
 
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
-#define LOG_TAG "android.hardware.power-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power-service.davinci-libperfmgr"
 
 #include "Power.h"
 

power-libperfmgr/aidl/PowerExt.cpp
@@ -15,7 +15,7 @@
  */
 
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
-#define LOG_TAG "android.hardware.power-service.pixel.ext-libperfmgr"
+#define LOG_TAG "android.hardware.power-service.davinci.ext-libperfmgr"
 
 #include "PowerExt.h"
 

power-libperfmgr/aidl/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power-service.davinci-libperfmgr"
 
 #include <thread>
 

power-libperfmgr/disp-power/InteractionHandler.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@-service.davinci-libperfmgr"
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>

power-libperfmgr/disp-power/display-helper.cpp
@@ -15,7 +15,7 @@
  */
 
 #define LOG_NIDEBUG 0
-#define LOG_TAG "android.hardware.power@-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@-service.davinci-libperfmgr"
 
 #include <dlfcn.h>
 #include <errno.h>

flawnn/device_oneplus_sm8150-common

Signed-off-by: baalajimaestro <me@baalajimaestro.me>

power-libperfmgr/aidl/Power.cpp
@@ -15,7 +15,7 @@
  */
 
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
-#define LOG_TAG "android.hardware.power-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power-service.msmnile-libperfmgr"
 
 #include "Power.h"
 

power-libperfmgr/aidl/PowerExt.cpp
@@ -15,7 +15,7 @@
  */
 
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
-#define LOG_TAG "android.hardware.power-service.pixel.ext-libperfmgr"
+#define LOG_TAG "android.hardware.power-service.msmnile.ext-libperfmgr"
 
 #include "PowerExt.h"
 

power-libperfmgr/aidl/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power-service.msmnile-libperfmgr"
 
 #include <thread>
 

power-libperfmgr/disp-power/InteractionHandler.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@-service.msmnile-libperfmgr"
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>

power-libperfmgr/disp-power/display-helper.cpp
@@ -15,7 +15,7 @@
  */
 
 #define LOG_NIDEBUG 0
-#define LOG_TAG "android.hardware.power@-service.pixel-libperfmgr"
+#define LOG_TAG "android.hardware.power@-service.msmnile-libperfmgr"
 
 #include <dlfcn.h>
 #include <errno.h>